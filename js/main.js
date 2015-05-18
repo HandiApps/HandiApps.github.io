b3939b160f048ebba21a7626cf8197825c97a4d6
JoseC <n3ggro@gmail.com>
2015-05-18 16:35:56 -0300

    project+contact

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 $( document ).ready(function() {
-  var distance = $('#title').offset().top, $window = $(window);
+  // var distance = $('#nav').offset().top, $window = $(window);
+  //
+  // $window.scroll(function() {
+  //     if ( $window.scrollTop() >= distance ) {
+  //       $('#navtitle').fadeIn();
+  //     }
+  //     else {
+  //       $('#navtitle').fadeOut();
+  //     }
+  // });
 
-  $window.scroll(function() {
-      if ( $window.scrollTop() >= distance ) {
-        $('#nav>.menu').fadeIn().removeClass('hidden');
-      }
-      else {
-        $('#nav>.menu').fadeOut();
-      }
+  $('#nav')
+  .sticky({
+    context: '#main'
   });
 });
