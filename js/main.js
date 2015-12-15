3d076f1083752ce51a9298963e3fa0d1255e9ff3
José Castro <n3ggro@gmail.com>
2015-12-15 15:26:22 -0300

    UPDATE feedback

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $( document ).ready(function() {
-  // var distance = $('#nav').offset().top, $window = $(window);
-  //
-  // $window.scroll(function() {
-  //     if ( $window.scrollTop() >= distance ) {
-  //       $('#navtitle').fadeIn();
-  //     }
-  //     else {
-  //       $('#navtitle').fadeOut();
-  //     }
-  // });
+  var distance = $('#nav').offset().top, $window = $(window);
+  
+  $window.scroll(function() {
+      if ( $window.scrollTop() >= distance ) {
+        $('#navtitle').fadeIn();
+      }
+      else {
+        $('#navtitle').fadeOut(200);
+      }
+  });
 
   $('#nav')
   .sticky({
